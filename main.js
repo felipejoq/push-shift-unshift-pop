fbfe9398e0f8bb46a08e24b00f7edbd704f49285
FelipeJ <felipejofreq@gmail.com>
2018-12-27 11:25:09 -0300

    Uso de los métodos push(), pop(), shift(), unshift() en javascript

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('Probando métodos push(), shift(), pop(), unshift()');
+
+// #### DECLARACIÓN DEL ARREGLO
+var animales = ['perro', 'gato'];
+console.log('Arreglo a manejar: ', animales);
+
+// Push para agregar elefante al final del arreglo
+console.log('Aplicando método push()');
+animales.push('elefante');
+console.log(animales);
+
+// Pop para eliminar el último elemento del arreglo.
+console.log('Aplicando método pop()');
+animales.pop();
+console.log(animales);
+
+// Unshift() para agregar un elemento al principio del arreglo.
+console.log('Aplicando método unshift()');
+animales.unshift('jirafa');
+console.log(animales);
+
+// Pop para quitar el primer elemento de un arreglo.
+console.log('Aplicando método pop()');
+animales.pop();
+console.log(animales);
+
